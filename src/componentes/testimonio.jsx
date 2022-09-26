83759a0f9a14a9d6059b63b8a6127e77389c64d7
Emill <lemito66@gmail.com>
2022-09-26 10:52:03 -0500

    Cambios a clases hechos en testimonio.jsx y app.js

--- src/componentes/testimonio.jsx
+++ src/componentes/testimonio.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import '../hojas-de-estilo/testimonio.css';
-function Testimonio(props) {
-	return (
-		/* Se necesita poner el require y todo eso dentro de llaves  */
-		<div className="contenedor-testimonio">
-			<img 
-				className="imagen-testimonio" 
-				src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-				alt={`Foto de ${props.fotoDe}`} /> 
-			<div className="contenedor-texto-testimonio">
-				<p className="nombre-testimonio">
-					<strong>{props.nombre}</strong> en {props.pais}
-				</p>
-				<p className="cargo-testimonio">
-					{props.cargo} en <strong>{props.empresa}</strong>
-				</p>
-				<p className="texto-testimonio">"{props.testimonio}"</p>
-			</div>
-		</div>
-		
-	);
+import "../hojas-de-estilo/testimonio.css";
+class Testimonio extends React.Component {
+  render() {
+    return (
+      /* Se necesita poner el require y todo eso dentro de llaves  */
+      <div className="contenedor-testimonio">
+        <img
+          className="imagen-testimonio"
+          src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+          alt={`Foto de ${this.props.fotoDe}`}
+        />
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio">
+            <strong>{this.props.nombre}</strong> en {this.props.pais}
+          </p>
+          <p className="cargo-testimonio">
+            {this.props.cargo} en <strong>{this.props.empresa}</strong>
+          </p>
+          <p className="texto-testimonio">"{this.props.testimonio}"</p>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Testimonio;
